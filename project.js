1bc7e1cfe250c7f2881caafbb77a45bab5ff6bf8
unknown <iltfhuss@gmail.com>
2024-09-22 23:26:45 +0500

    collect the bet amount in the file

--- project.js
+++ project.js
@@ -15,9 +15,10 @@ const deposit = () => {
         const depositAmount = prompt("Enter a deposit amount: ");
         const numberDepositAmount = parseFloat(depositAmount);
 
-        if(isNaN(numberDepositAmount) || numberDepositAmount >= 0){
-            console.log("Invalid deposit amount, try Again!")
+        if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid deposit amount, try Again!");
         }
+
         else{
             return numberDepositAmount;
         }
@@ -25,5 +26,39 @@ const deposit = () => {
 };
 
 
-const depositAmount = deposit();
-console.log(depositAmount);
+const getNumberOfLines = () => {
+    while(Ture){
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Invalid deposit amount, try Again!");
+        }
+
+        else{
+            return numberOfLines;
+        }
+    }
+}
+
+
+const getBet = (balance, lines) => {
+    while(True){
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines){
+            console.log("Invalid bet, try again!");
+        }
+
+        else{
+            return numberBet;
+        }
+    }
+
+};
+
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
