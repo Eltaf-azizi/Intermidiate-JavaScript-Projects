3c12dc50dacdfa26cc598174831a3352248c15ae
unknown <iltfhuss@gmail.com>
2024-09-23 23:56:58 +0500

    added code

--- project.js
+++ project.js
@@ -88,19 +88,37 @@ const spin = () => {
     }
     
 
-    const reels = [[], [], []];
+    const reels = [];
     for(let i=0; i<COLS; i++) {
+        reels.push([]);
         const reelSymbols = [...symbols];
         for(let j=0; j<ROWS; j++){
 
             const randomIndex = Math.floor(Math.random() * reelSymbols.length);
             const selectedSymbol = reelSymbols[randomIndex];
+
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
         }
     }
+
+    return reels;
 };
 
 
-spin()
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for(let i = 0; i < ROWS; i++){
+        rows.push([]);
+    }
+}
+
+
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin()
+
