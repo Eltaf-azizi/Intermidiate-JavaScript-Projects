674596691f9b88e0a55700d8ed62cfe4bf9e46aa
unknown <iltfhuss@gmail.com>
2024-09-24 00:17:58 +0500

    added code

--- project.js
+++ project.js
@@ -112,13 +112,35 @@ const transpose = (reels) => {
 
     for(let i = 0; i < ROWS; i++){
         rows.push([]);
+        for(let j=0; j< COLS; j++){
+            rows[i].push(reels[j][i])
+        }
+    }
+
+    return rows;
+};
+
+
+const printRows = (rows) => {
+
+    for(const row of rows) {
+        let rowString = "A";
+
+        for(const [i, symbol] of row.entries()) {
+            rowString += symbol
+
+            if(i != row.length - 1) {
+                rowString += " | "
+            }
+        }
+        console.log(rowString)
     }
 }
-
 
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 const reels = spin()
+const rows = transpose(reels)
 
