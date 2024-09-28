96338dfe59b19ad2f4c2b4897ffaf21c021212d0
unknown <iltfhuss@gmail.com>
2024-09-28 23:39:10 +0500

    adding Todos and using props

---
+++ todoList/src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const TodoForm = () => {
+    return (
+        <form className='TodoForm'>
+            <input type="text" className="todo-input" placeholder="What is the task today?"/>
+            <button type='submit' className='todo-btn'>
+                
+            </button>
+        </form>
+    )
+}
---
+++ todoList/src/components/editTodoForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const editTodoForm = () => {
+    return (
+        <div>editTodoForm</div>
+    )
+}
