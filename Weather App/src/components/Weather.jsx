41c3738a574bf3ed53a5268ac86e70e686c29b6d
unknown <iltfhuss@gmail.com>
2024-10-07 20:03:43 +0500

    created weather app

--- Weather App/src/components/Weather.jsx
+++ Weather App/src/components/Weather.jsx
@@ -1,69 +1,113 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.jpg'
 import sunny_icon from '../assets/sunny.jpg'
 import humidity_icon from '../assets/humidity.jpg'
 import wind_icon from '../assets/wind.jpg'
-import search_icon from '../assets/search.jpg'
-import search_icon from '../assets/search.jpg'
-import search_icon from '../assets/search.jpg'
+import drizzle_icon from '../assets/search.jpg'
+import rain_icon from '../assets/search.jpg'
+import snow_icon from '../assets/search.jpg'
+import cloud_icon from '../assets/search.jpg'
 
 const Weather = () => {
 
+  const imputRef = useRef()
   const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon
+  }
+
+
   const search = async(city) => {
+
+    if(city === "") {
+
+      alert("Enter City Name");
+      return;
+
+    }
+
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
       const response = await fetch(url);
       const data = await response.json();
+
+      if(!response.ok) {
+        alert(data.message);
+        return;
+      }
+
+
       console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
       setWeatherData({
         humidity: data.main.humidity,
         windSpeed: data.wind.speed,
-        temperature: data.main.temp
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon
 
       })
 
     }catch(error) {
-
+      setWeatherData(false);
+      console.error("Error in fetchin weather data");
     }
   }
 
   useEffect(() => {
+
     search("London")
   
-    return () => {
-      second
-    }
-  }, [third])
+
+  }, [])
   
   
   return (
     <div className='weather'>
         <div className="search-bar">
-            <input type="text" placeholder='Search' />
-            <img src={search_icon} alt="" />
+            <input ref={inputRef} type="text" placeholder='Search' />
+            <img src={search_icon} alt="" onClick={()=> search(inputRef.current.value)}/>
         </div>
-        <img src={sunny_icon} alt="" className='weather-icon' />
-        <p className='temperature'>16°c</p>
-        <p className='location'>London</p>
-        <div className="weather-data">
-          <div className="col">
-            <img src={humidity_icon} alt="" />
-            <div>
-              <p>91</p>
-              <span>Humidity</span>
+
+        {weatherData?<>
+          <img src={weatherData.icon} alt="" className='weather-icon' />
+          <p className='temperature'>{weatherData.temperature}°c</p>
+          <p className='location'>{weatherData}</p>
+          <div className="weather-data">
+            <div className="col">
+              <img src={humidity_icon} alt="" />
+              <div>
+                <p>{weatherData.humidity}</p>
+                <span>Humidity</span>
+              </div>
+            </div>
+            <div className="col">
+              <img src={wind_icon} alt="" />
+              <div>
+                <p>{weatherData.windSpeed} Km/h</p>
+                <span>Wind Speed</span>
+              </div>
             </div>
           </div>
-          <div className="col">
-            <img src={wind_icon} alt="" />
-            <div>
-              <p>3.7 Km/h</p>
-              <span>Wind Speed</span>
-            </div>
-          </div>
-        </div>
+        </>:<></>}
+
+        
     </div>
   )
 }
